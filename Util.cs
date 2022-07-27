2d08a6f01415a1c03395a0f8b5b467dbf6f114c8
KW <kenneth_sl_wong@msn.com>
2022-07-27 19:15:37 -0400

    added function to add picture onto badge

--- Util.cs
+++ Util.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Drawing;
+using System.Net;
 using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
@@ -28,6 +30,56 @@ namespace CatWorx.BadgeMaker
                 {
                     string template = "{0},{1},{2}";
                     file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+                }
+            }
+        }
+
+        public static void MakeBadges(List<Employee> employees)
+        {
+            int BADGE_WIDTH = 669;
+            int BADGE_HEIGHT = 1044;
+
+            int COMPANY_NAME_START_X = 0;
+            int COMPANY_NAME_START_Y = 110;
+            int COMPANY_NAME_WIDTH = 100;
+
+            int PHOTO_START_X = 184;
+            int PHOTO_START_Y = 215;
+            int PHOTO_WIDTH = 302;
+            int PHOTO_HEIGHT = 302;
+
+            int EMPLOYEE_NAME_START_X = 0;
+            int EMPLOYEE_NAME_START_Y = 560;
+            int EMPLOYEE_NAME_WIDTH = BADGE_WIDTH;
+            int EMPLOYEE_NAME_HEIGHT = 100;
+
+            int EMPLOYEE_ID_START_X = 0;
+            int EMPLOYEE_ID_START_Y = 690;
+            int EMPLOYEE_ID_WIDTH = BADGE_WIDTH;
+            int EMPLOYEE_ID_HEIGHT = 100;
+
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+            int FONT_SIZE = 32;
+            Font font = new Font("Arial", FONT_SIZE);
+            Font monoFont = new Font("Courier New", FONT_SIZE);
+            SolidBrush brush = new SolidBrush(Color.Black);
+
+            using(WebClient client = new WebClient())
+            {
+                for(int i = 0; i < employees.Count; i++)
+                {
+                    Stream employeeStream = client.OpenRead(employees[i].GetPhotoUrl());
+                    Image photo = Image.FromStream(employeeStream);
+                    Image background = Image.FromFile("badge.png");
+
+                    Image badge = new Bitmap(BADGE_WIDTH, BADGE_HEIGHT);
+                    Graphics graphic = Graphics.FromImage(badge);
+
+                    graphic.DrawImage(background, new Rectangle(0,0,BADGE_WIDTH, BADGE_HEIGHT));
+                    graphic.DrawImage(photo, new Rectangle(PHOTO_START_X, PHOTO_START_Y, PHOTO_WIDTH, PHOTO_HEIGHT));
+
+                    badge.Save("data/employeeBadge.png");
                 }
             }
         }
