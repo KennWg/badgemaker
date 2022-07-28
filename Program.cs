ac399027a8904522968d657747b3c30d99cc0977
KW <kenneth_sl_wong@msn.com>
2022-07-27 21:47:46 -0400

    added api call for badge creation

--- Program.cs
+++ Program.cs
@@ -5,35 +5,9 @@ namespace CatWorx.BadgeMaker
 {
   class Program
 {
-  static List<Employee> GetEmployees()
-  {
-    List<Employee> employees = new List<Employee>();
-    while (true)
-    {
-
-      Console.WriteLine("Please enter a name: (leave empty to exit): ");
-
-      string firstName = Console.ReadLine();
-
-      if (firstName == "")
-      {
-        break;
-      }
-      Console.Write("Enter last name: ");
-        string lastName = Console.ReadLine();
-        Console.Write("Enter ID: ");
-        int id = Int32.Parse(Console.ReadLine());
-        Console.Write("Enter Photo URL:");
-        string photoUrl = Console.ReadLine();
-      Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-      employees.Add(currentEmployee);
-    }
-    return employees;
-  }
-
   static void Main(string[] args)
   {
-    List<Employee> employees = GetEmployees();
+    List<Employee> employees = PeopleFetcher.GetFromApi();
     Util.PrintEmployees(employees);
     Util.MakeCSV(employees);
     Util.MakeBadges(employees);
